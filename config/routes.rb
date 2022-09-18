abf3137ebac75ee016ca0a59b02a91151bc85184
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-09-18 12:33:18 -0500

    Fin

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :tasks
+  root to: "tasks#index"
+
+  # #* create
+  # get "tasks", to: "tasks#index" #? Read all
+  # post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+
+  # #* read
+  # get "tasks/:id", to: "tasks#show", as: :task #? Read by id
+
+  # #* update
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # #* delete
+  # delete "tasks/:id", to: "tasks#destroy"
 end
